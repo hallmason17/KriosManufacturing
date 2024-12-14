8d116cb76c5a2576fbfe0a66b0259f4a8c4ec0a8
Mason Hall <hallmason17@gmail.com>
2024-12-14 17:25:41 -0500

    add item to lot, add items index page

---
+++ KriosManufacturing.Core/Migrations/20241214220222_AddItemToLot.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KriosManufacturing.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddItemToLot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ItemId",
+                table: "Lots",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lots_ItemId",
+                table: "Lots",
+                column: "ItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lots_Items_ItemId",
+                table: "Lots",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lots_Items_ItemId",
+                table: "Lots");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Lots_ItemId",
+                table: "Lots");
+
+            migrationBuilder.DropColumn(
+                name: "ItemId",
+                table: "Lots");
+        }
+    }
+}
--- KriosManufacturing.Core/Models/Lot.cs
+++ KriosManufacturing.Core/Models/Lot.cs
@@ -7,7 +7,11 @@ public class Lot
 {
     public long Id { get; set; }
 
+    public long ItemId { get; set; }
+
     public required string LotNumber { get; set; }
 
     public DateOnly ExpirationDate { get; set; }
+
+    public required Item Item { get; set; }
 }
